fcf69fab8b9b729009013d48562cb9f81d925c6b
Priyanka-saw <priyankasaw51@gmail.com>
2025-10-22 19:21:42 +0530

    blog project completed

---
+++ models/comments.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose');
+
+const commentSchema = new Schema({
+    body: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    blogId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Blog',
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const Comment  = model('Comment', commentSchema);
+
+module.exports = Comment;
--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,7 @@ const router = Router();
 const path = require('path');
 
 const Blog = require('../models/blog');
+const Comment = require('../models/comments');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -27,13 +28,27 @@ router.get('/add-new', (req, res) => {
 
 
 router.get('/:id', async (req, res) => {
-    const blog = await Blog.findById(req.params.id);
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
+    const comments = await Comment.find({ blogId: req.params.id }).populate('createdBy'
+
+    );
+    
     return res.render('blog', {
         user: req.user,
         blog,
+        comments,
     });
 });
 
+
+router.post('/comment/:blogId', async (req, res) => {
+     await Comment.create({
+        body: req.body.body,
+        createdBy: req.user._id,
+        blogId: req.params.blogId,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
 
 
 router.post('/', upload.single('coverImage'), async (req, res) => {
